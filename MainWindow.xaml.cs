9aef9c1d4ce1244e4768498a133714717bd7917b
Artem Karpov <artem_karpov@lesta.group>
2025-08-14 17:35:10 +0300

    bg + move

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,10 +14,19 @@ namespace vider;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
+
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    private void Move(object sender, RoutedEventArgs e)
+    {
+        if (Mouse.LeftButton == MouseButtonState.Pressed)
+        {
+            DragMove();
+        }
     }
 }
